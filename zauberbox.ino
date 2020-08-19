87f5e4625390b8c54593c64f3151a45b56ba72ef
Jan Grosser <email@jan-grosser.de>
2020-08-19 18:48:13 +0200

    Improved code style - #define for constants

--- zauberbox.ino
+++ zauberbox.ino
@@ -4,6 +4,9 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+// Speed of serial port
+#define SERIAL_BAUDRATE 9600
+
 /* Define SW UART for GPS receiver
 *
 *  GPSr Pin  | Arduino Pin
@@ -12,7 +15,10 @@
 *  TX        | D4
 *
 */
-NeoSWSerial gpsSerial(3, 4);
+#define GPS_RX_PIN 3
+#define GPS_TX_PIN 4
+#define GPS_BAUDRATE 9600
+NeoSWSerial gpsSerial(GPS_RX_PIN, GPS_TX_PIN);
 TinyGPSPlus gps;
 
 // Because gps.satellites.value() from TinyGPS++ does not work as
@@ -21,21 +27,34 @@ TinyGPSPlus gps;
 TinyGPSCustom nmea_nsat(gps, "GPGSV", 3);
 
 // Define servo
+// Servo attached to pin D9 of microcontroller
+#define SERVO_PIN 9
+#define SERVO_INIT_POS 90
 Servo servo1;
+
+// Define piezo
+// Piezo attached to pin D6 of microcontroller
+#define PIEZO_PIN 6
 
 /* Define LCD
 *  I2C address 0x27
 *  16 columns
 *  2 rows
 */
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define LCD_I2C_ADDR 0x27
+#define LCD_COLS 16
+#define LCD_ROWS 2
+LiquidCrystal_I2C lcd(LCD_I2C_ADDR, LCD_COLS, LCD_ROWS);
 
 // Define some variables for GPS data
 float lat, lon;
 int year, nsat;
 byte month, day, hour, minute, second;
 
+// Function to display/update time on LCD
+// HH:MM:SS
 void lcd_print_time(byte hour, byte minute, byte second) {
+    // Print time in upper-left corner
     lcd.setCursor(0, 0);
     if (hour < 10) { lcd.print("0"); }
     lcd.print(hour);
@@ -47,7 +66,10 @@ void lcd_print_time(byte hour, byte minute, byte second) {
     lcd.print(second);
 }
 
+// Function to display/update number of satellites on LCD
+// [NN]
 void lcd_print_nsat(int nsat) {
+    // Print nsat to upper-right corner
     lcd.setCursor(12, 0);
     lcd.print("[");
     if (nsat < 10) { lcd.print("0"); }
@@ -58,17 +80,17 @@ void lcd_print_nsat(int nsat) {
 // Setup Routine
 void setup() {
     // Setup the serial port to computer
-    Serial.begin(9600);
+    Serial.begin(SERIAL_BAUDRATE);
     Serial.print("Starting . . .\r\n");
     Serial.print("  UART to computer [ok]\r\n");
 
     // Setup the serial port to GPSr
-    gpsSerial.begin(9600);
+    gpsSerial.begin(GPS_BAUDRATE);
     Serial.print("  UART to GPSr [ok]\r\n");
 
     // Setup the servo
-    servo1.attach(9);
-    servo1.write(90);
+    servo1.attach(SERVO_PIN);
+    servo1.write(SERVO_INIT_POS);
     Serial.print("  Servo [ok]\r\n");
 
     // Setup LCD
@@ -81,7 +103,7 @@ void setup() {
     Serial.print("  LCD [ok]\r\n");
 
     // Setup output pin for piezo
-    pinMode(6, OUTPUT);
+    pinMode(PIEZO_PIN, OUTPUT);
 }
 
 // Main loop
